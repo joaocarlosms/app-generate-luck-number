06a845a5ea7c47847b69fef672acef55ab79019c
joaocarlosms <jcmeseverino@gmail.com>
2024-07-23 15:24:18 -0300

    first commit

--- app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
@@ -1,11 +1,73 @@
 package co.tiagoaguiar.ganheinamega
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var prefs: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        //buscar objects e ter referencia deles
+        val editText: EditText = findViewById(R.id.edit_number)
+        val txtResult: TextView = findViewById(R.id.txt_result)
+        val btnGenerate: Button = findViewById(R.id.btn_generate)
+
+        prefs = getSharedPreferences("db", Context.MODE_PRIVATE)
+        val results = prefs.getString("results", null)
+
+        if(results != null) {
+            txtResult.text = "ultima aposta: $results"
+        }
+
+        //eventos de touch:
+        btnGenerate.setOnClickListener {
+            val text = editText.text.toString()
+
+            numberGenerator(text, txtResult)
+        }
+    }
+
+    private fun numberGenerator(text: String, txtResult: TextView) {
+        if (text.isEmpty()) {
+            Toast.makeText(this, "Informe um numero entre 6 e 15", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val qntNumbers = text.toInt()
+
+        if (qntNumbers < 6 || qntNumbers > 15) {
+            Toast.makeText(this, "Informe um numero entre 6 e 15", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val numbers = mutableSetOf<Int>()
+        val random = Random()
+
+        while (true) {
+            val number = random.nextInt(60) //0...59
+            numbers.add(number + 1) //incrementa um pq queremos pegar os numbers de 1 ate 60
+
+            if (numbers.size == qntNumbers) {
+                break
+            }
+        }
+
+        txtResult.text = numbers.joinToString(" - ")
+
+        val editor = prefs.edit()
+        editor.putString("results", txtResult.text.toString())
+        editor.apply()
     }
 }
